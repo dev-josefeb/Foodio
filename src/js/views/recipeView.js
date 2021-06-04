3db2f7a0b3eef0dfd6f568a5bb54734b8fbc9baf
dev-josefeb <febinjose@protonmail.com>
2021-06-04 12:40:51 +0200

    Add fractional library to format ingredient quantities

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,5 @@
 import icons from 'url:../../img/icons.svg';
+import { Fraction } from 'fractional';
 
 class RecipeView {
   #data;
@@ -81,22 +82,7 @@ class RecipeView {
     <div class="recipe__ingredients">
       <h2 class="heading--2">Recipe ingredients</h2>
       <ul class="recipe__ingredient-list">
-        ${this.#data.ingredients
-          .map(ingredient => {
-            return `
-          <li class="recipe__ingredient">
-          <svg class="recipe__icon">
-            <use href="${icons}#icon-check"></use>
-          </svg>
-          <div class="recipe__quantity">${ingredient.quantity}</div>
-          <div class="recipe__description">
-            <span class="recipe__unit">${ingredient.unit}</span>
-            ${ingredient.description}
-          </div>
-        </li>
-          `;
-          })
-          .join('')}
+        ${this.#data.ingredients.map(this._generateIngredientsMarkup).join('')}
       </ul>
     </div>
 
@@ -119,6 +105,21 @@ class RecipeView {
       </a>
     </div>
   `;
+  }
+
+  _generateIngredientsMarkup(ingredient) {
+    return `
+    <li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="${icons}#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">${ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''}</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">${ingredient.unit}</span>
+        ${ingredient.description}
+      </div>
+    </li>
+    `;
   }
 }
 
