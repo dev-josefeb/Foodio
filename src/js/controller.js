8ca15d3e1c419acc2435056c10164d71dac6cd74
dev-josefeb <febinjose@protonmail.com>
2021-06-04 11:16:55 +0200

    Render loading spinner

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,11 +11,26 @@ const timeout = function (s) {
   });
 };
 
+renderSpinner = function (parentEl) {
+  const markup = `
+    <div class="spinner">
+      <svg>
+        <use href="${icons}#icon-loader"></use>
+      </svg>
+    </div> 
+  `;
+
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
+
 async function showRecipe() {
   try {
-    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    renderSpinner(recipeContainer);
 
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
     const data = await res.json();
+
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
 
     let { recipe } = data.data;
