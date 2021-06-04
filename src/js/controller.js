2fa098310ddc8fa74bd58efb4a1627ea1626be74
dev-josefeb <febinjose@protonmail.com>
2021-06-04 13:53:27 +0200

    Add helper functions and config file

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,14 +4,6 @@ import recipeView from './views/recipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-function timeout(s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-}
-
 async function controlRecipes() {
   try {
     const id = window.location.hash.slice(1);
--- src/js/model.js
+++ src/js/model.js
@@ -1,15 +1,16 @@
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+
 export const state = {
   recipe: {},
 };
 
 export async function loadRecipe(id) {
   try {
-    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
+
     state.recipe = {
       id: recipe.id,
       title: recipe.title,
@@ -20,9 +21,7 @@ export async function loadRecipe(id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
-
-    console.log(state.recipe);
   } catch (error) {
-    alert(error);
+    console.log(error);
   }
 }
